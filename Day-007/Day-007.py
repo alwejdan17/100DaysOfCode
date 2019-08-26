844eb93cbe23f58da474bd3878ffad49c52d25f5
alwejdan17 <wejdanalharby.com>
2019-08-27 02:46:38 +0300

    Day-007-String

---
+++ Day-007/Day-007.py
@@ -0,0 +1,18 @@
+
+
+print("\nDay-007 About String in Python\n")
+
+name="WEJDAN"
+print("Name:",name,"\n")
+
+print("First three letters in",name,"\n",name[0:3],"\n")
+
+print("Last three letters in ",name,"\n",name[-3:],"\n")
+
+
+text_str="""
+In this Lesson We learned about string\n
+1. The string is an array and we can access to a character or characters using index by use []
+   name [-3:] >> we accessed to last 3 letters in this array\n
+2. we can also assign string in multi-lines by using three double quotes Or three single quotes."""
+print(text_str)
