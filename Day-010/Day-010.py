e9ca5ee3a35ea46501a34976a9e75d295c70cb16
alwejdan17 <wejdanalharby.com>
2019-08-29 22:25:03 +0300

    Day-010-operators-1

---
+++ Day-010/Day-010.py
@@ -0,0 +1,44 @@
+
+#In this lesson we learned about Python Operators
+#------------- 1.Arithmetic operators.--------------
+#+ - * / // ** %
+a=12
+b=2
+print("----1.Arithmetic operators.----\n")
+print("a = {0} , and b = {1}".format(a,b))
+print("1.Addition operation a+b ={0}\n".format(a+b))
+print("2.Subtraction operation a-b ={0}\n".format(a-b))
+print("3.Multiplication operation a*b ={0}\n".format(a*b))
+print("4.Division operation a/b ={0}\n".format(a/b))
+print("5.Floor division operation a//b ={0}\n".format(a//b))
+print("6.Modulus operation  a mod b ={0}\n".format(a%b))
+print("7.Power (exponentiation)operation a**b  ={0}\n".format(a**b))
+print("------------------------------------------------")
+#-------------2.Assignment operators.--------------
+print("----2.Assignment operators.----\n")
+print("a = {0}\n".format(a))
+a+=10
+print("1.Addition Assignment a+=10  , new a = {0} \n".format(a))
+a-=3
+print("2.Subtraction Assignment a-=3  , new a = {0} \n".format(a))
+a*=20
+print("3.Multiplication Assignment a*=20  , new a = {0} \n".format(a))
+a/=2
+print("4.Division Assignment a/=2  , new a = {0} \n".format(a))
+a/=2
+print("5.Floor division  Assignment a//=2  , new a = {0} \n".format(a))
+a%=3
+print("6.Modulus Assignment a%=3  , new a = {0} \n".format(a))
+a**=3
+print("7.Power Assignment a**=3  , new a = {0} \n".format(a))
+print("------------------------------------------------")
+#-------------3.Comparison operators.--------------
+print("----3.Comparison operators.----\n")
+print("a = {0} , and b = {1}".format(a,b))
+print("1.Equal  a==b ? {0} \n".format(a==b))
+print("2.Not equal  a!=b ? {0} \n".format(a!=b))
+print("3.greater than  a>b ? {0} \n".format(a>b))
+print("4.greater than or equal  a>=b ? {0} \n".format(a>=b))
+print("5.less than  a<b ? {0} \n".format(a<b))
+print("6.less than or equal a<=b ? {0} \n".format(a<=b))
+
