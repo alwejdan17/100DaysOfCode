482e78265338617c77f7e2805072ecfa184eb98c
alwejdan17 <wejdanalharby.com>
2019-09-07 20:24:31 +0300

    Day-019-A practicing on Tuples

---
+++ Day-019/Day-019.py
@@ -0,0 +1,8 @@
+
+list_lang=("java", "python", "swift")
+print("Tuples of programming languages",list_lang)
+print("python in Tuple of programming languages? ")
+if "python" in list_lang:
+    print("Yes :)")
+else:
+    print("NO :'(")
